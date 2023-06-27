74cc8ce1f5351108f4914722975ab219403d5dc7
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-06-27 17:36:01 +0900

    initial commit

---
+++ src/main/java/com/gsm/blabla/BlablaApplication.java
@@ -0,0 +1,13 @@
+package com.gsm.blabla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlablaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlablaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gsm/blabla/BlablaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsm.blabla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlablaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
