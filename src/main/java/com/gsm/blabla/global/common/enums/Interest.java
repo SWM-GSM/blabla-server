4982462e71ca777a94e5b033a31fd320d1144764
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-07-08 22:02:03 +0900

    fix: 키워드 조회 API 영어 버전 추가

--- src/main/java/com/gsm/blabla/global/common/enums/Interest.java
+++ src/main/java/com/gsm/blabla/global/common/enums/Interest.java
@@ -5,43 +5,44 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Interest {
-    MOVIE("엔터테인먼트", "\uD83C\uDFAC 영화 감상"),
-    NETFLIX("엔터테인먼트", "\uD83C\uDF7F 넷플릭스"),
-    KPOP("엔터테인먼트", "\uD83C\uDFA4 K-POP"),
-    ANIMATION("엔터테인먼트", "\uD83D\uDC7B 애니메이션"),
-    INSTAGRAM("엔터테인먼트","✨ 인스타그램"),
-    GAME("엔터테인먼트", "\uD83C\uDFAE 게임"),
-    YOUTUBE("엔터테인먼트", "\uD83D\uDCFD️ 유튜브"),
-    DRAMA("엔터테인먼트", "\uD83E\uDD20 드라마"),
-    WEBTOON("엔터테인먼트", "\uD83C\uDF10 웹툰"),
-    SINGING("엔터테인먼트", "\uD83C\uDF99️ 노래 부르기"),
-    PLAYING_INSTRUMENT("엔터테인먼트", "\uD83E\uDD41 악기 연주"),
-    MUKBANG("엔터테인먼트", "\uD83C\uDF7D️ 먹방"),
-    WATCHING_PERFORMANCE("엔터테인먼트", "\uD83C\uDFBA 공연 관람"),
+    FILM("엔터테인먼트", "\uD83C\uDFAC 영화 감상", "\uD83C\uDFAC Film"),
+    NETFLIX("엔터테인먼트", "\uD83C\uDF7F 넷플릭스", "\uD83C\uDF7F Netflix"),
+    KPOP("엔터테인먼트", "\uD83C\uDFA4 K-Pop", "\uD83C\uDFA4 K-Pop"),
+    ANIMATION("엔터테인먼트", "\uD83D\uDC7B 애니메이션", "\uD83D\uDC7B Animation"),
+    INSTAGRAM("엔터테인먼트","✨ 인스타그램", "✨ Instagram"),
+    GAME("엔터테인먼트", "\uD83C\uDFAE 게임", "\uD83C\uDFAE Game"),
+    YOUTUBE("엔터테인먼트", "\uD83D\uDCFD️ 유튜브", "\uD83D\uDCFD️ Youtube"),
+    DRAMA("엔터테인먼트", "\uD83E\uDD20 드라마", "\uD83E\uDD20 Drama"),
+    WEBTOON("엔터테인먼트", "\uD83C\uDF10 웹툰", "\uD83C\uDF10 Webtoon"),
+    SING("엔터테인먼트", "\uD83C\uDF99️ 노래 부르기", "\uD83C\uDF99️ Sing"),
+    INSTRUMENTS("엔터테인먼트", "\uD83E\uDD41 악기 연주", "\uD83E\uDD41 Instruments"),
+    MUKBANG("엔터테인먼트", "\uD83C\uDF7D️ 먹방", "\uD83C\uDF7D️ Mukbang"),
+    CONCERT("엔터테인먼트", "\uD83C\uDFBA 공연 관람", "\uD83C\uDFBA Concert"),
 
-    INTROVERT("성격", "\uD83D\uDE0C 내향적인"),
-    OPTIMISTIC("성격", "\uD83D\uDE0A 낙천적인"),
-    EMOTIONAL("성격", "\uD83D\uDE07 감성적인"),
-    EXTROVERTED("성격", "\uD83D\uDE06 외향적인"),
-    HUMOROUS("성격", "\uD83D\uDE1D 유머러스"),
+    INTROVERTED("성격", "\uD83D\uDE0C 내향적인", "\uD83D\uDE0C Introverted"),
+    OPTIMISTIC("성격", "\uD83D\uDE0A 낙천적인", "\uD83D\uDE0A Optimistic"),
+    EMOTIONAL("성격", "\uD83D\uDE07 감성적인", "\uD83D\uDE07 Emotional"),
+    OUTGOING("성격", "\uD83D\uDE06 외향적인", "\uD83D\uDE06 Outgoing"),
+    HUMOROUS("성격", "\uD83D\uDE1D 유머러스", "\uD83D\uDE1D Humorous"),
 
-    TRAVEL("취미/관심사", "✈️ 여행"),
-    LANGUAGE_EXCHANGE("취미/관심사", "\uD83D\uDCAC 언어교환"),
-    RELATIONSHIP("취미/관심사", "\uD83D\uDC81\u200D 대인관계"),
-    LANGUAGE("취미/관심사", "\uD83D\uDD8B️ 어학"),
-    READING("취미/관심사", "\uD83D\uDCDA 독서"),
-    DRIVE("취미/관심사", "\uD83D\uDE97 드라이브"),
-    SHOPPING("취미/관심사", "\uD83D\uDECD️ 쇼핑"),
-    COUNSELING("취미/관심사", "\uD83D\uDCEE 고민상담"),
-    COOKING("취미/관심사", "\u200D\uD83C\uDF73 요리"),
-    BAKING("취미/관심사", "\uD83E\uDD50 베이킹"),
-    DIY("취미/관심사", "\uD83E\uDDF6 DIY"),
-    SPORTS("취미/관심사", "⛳ 스포츠"),
-    CAREER("취미/관심사", "\uD83D\uDCBC 커리어"),
-    FINANCE("취미/관심사", "\uD83D\uDCB0 재테크")
+    TRAVEL("취미/관심사", "✈️ 여행", "✈️ Travel"),
+    LANGUAGE_EXCHANGE("취미/관심사", "\uD83D\uDCAC 언어교환", "\uD83D\uDCAC Language Exchange"),
+    PERSONAL_RELATIONS("취미/관심사", "\uD83D\uDC81\u200D 대인관계", "\uD83D\uDC81\u200D Personal Relations"),
+    LANGUAGE("취미/관심사", "\uD83D\uDD8B️ 어학", "\uD83D\uDD8B️ Language"),
+    BOOK("취미/관심사", "\uD83D\uDCDA 독서", "\uD83D\uDCDA Book"),
+    DRIVE("취미/관심사", "\uD83D\uDE97 드라이브", "\uD83D\uDE97 Drive"),
+    SHOPPING("취미/관심사", "\uD83D\uDECD️ 쇼핑", "\uD83D\uDECD️ Shopping"),
+    COUNSELING("취미/관심사", "\uD83D\uDCEE 고민상담", "\uD83D\uDCEE Counseling"),
+    COOK("취미/관심사", "\u200D\uD83C\uDF73 요리", "\u200D\uD83C\uDF73 Cook"),
+    BAKING("취미/관심사", "\uD83E\uDD50 베이킹", "\uD83E\uDD50 Baking"),
+    DIY("취미/관심사", "\uD83E\uDDF6 DIY", "\uD83E\uDDF6 DIY"),
+    SPORTS("취미/관심사", "⛳ 스포츠", "⛳ Sports"),
+    CAREER("취미/관심사", "\uD83D\uDCBC 커리어", "\uD83D\uDCBC Career"),
+    FINANCIAL("취미/관심사", "\uD83D\uDCB0 재테크", "\uD83D\uDCB0 Financial"),
     ;
 
     private final String category;
-    private final String name;
+    private final String koreanName;
+    private final String englishName;
 
 }
--- src/main/java/com/gsm/blabla/global/common/enums/api/EnumController.java
+++ src/main/java/com/gsm/blabla/global/common/enums/api/EnumController.java
@@ -6,7 +6,6 @@ import com.gsm.blabla.global.common.enums.Level;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.persistence.criteria.CriteriaBuilder.In;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,17 +13,15 @@ import java.util.Map;
 import java.util.Objects;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "공통 코드 관련 API")
 @RestController
-@RequestMapping("/common")
 public class EnumController {
 
     @Operation(summary = "레벨 별 문구 조회 API")
     @Parameter(name = "language", description = "언어", example = "ko / eng")
-    @GetMapping("/levels/{language}")
+    @GetMapping("common/levels/{language}")
     public DataResponseDto<Map<String, String>> getLevels(@PathVariable String language) {
         Map<String, String> map = new HashMap<>();
 
@@ -37,30 +34,39 @@ public class EnumController {
         return DataResponseDto.of(map);
     }
 
-    @Operation(summary = "관심사 조회 API")
-    @GetMapping("/interests")
-    public DataResponseDto<Map<String, List<String>>> getInterests() {
+    @Operation(summary = "키워드 조회 API")
+    @Parameter(name = "language", description = "언어", example = "ko / eng")
+    @GetMapping("/{language}/common/keywords")
+    public DataResponseDto<Map<String, List<String>>> getInterests(@PathVariable String language) {
         Map<String, List<String>> map = new HashMap<>();
 
-        List<String> interests = new ArrayList<>();
-        List<String> characteristics = new ArrayList<>();
-        List<String> hobbies = new ArrayList<>();
+        List<String> entertainments = makeKeywordList(language, "엔터테인먼트");
+        List<String> characteristics = makeKeywordList(language, "성격");
+        List<String> hobbies = makeKeywordList(language, "취미/관심사");
 
-        for (Interest interest : Interest.values()) {
-            if (Objects.equals(interest.getCategory(), "엔터테인먼트")) {
-                interests.add(interest.getName());
-            } else if (Objects.equals(interest.getCategory(), "성격")) {
-                characteristics.add(interest.getName());
-            } else {
-                hobbies.add(interest.getName());
-            }
-        }
-
-        map.put("엔터테인먼트", interests);
+        map.put("엔터테인먼트", entertainments);
         map.put("성격", characteristics);
         map.put("취미/관심사", hobbies);
 
         return DataResponseDto.of(map);
 
+    }
+
+    private List<String> makeKeywordList(String language, String category) {
+        List<String> keywords = new ArrayList<>();
+
+        for (Interest interest : Interest.values()) {
+            if (Objects.equals(language, "ko")) {
+                if (Objects.equals(interest.getCategory(), category)) {
+                    keywords.add(interest.getKoreanName());
+                }
+            } else {
+                if (Objects.equals(interest.getCategory(), category)) {
+                    keywords.add(interest.getEnglishName());
+                }
+            }
+        }
+
+        return keywords;
     }
 }
