b418c53bf8c0f1d0a718749abccbd009f56584a9
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-07-10 17:21:40 +0900

    refactor: MethodArgumentNotValidException에 대한 exception handler 추가

--- src/main/java/com/gsm/blabla/global/config/ExceptionHandler.java
+++ src/main/java/com/gsm/blabla/global/config/ExceptionHandler.java
@@ -6,7 +6,9 @@ import com.gsm.blabla.global.common.dto.ErrorResponseDto;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -28,6 +30,21 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
     @org.springframework.web.bind.annotation.ExceptionHandler
     public ResponseEntity<Object> exception(Exception e, WebRequest request) {
         return handleExceptionInternal(e, Code.INTERNAL_ERROR, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+        MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Code code = Code.VALIDATION_ERROR;
+
+        String errorMessage = ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+        String message = code.getMessage() + " - " + errorMessage;
+
+        return handleExceptionInternal(ex,
+            ErrorResponseDto.of(code, message),
+            headers,
+            status,
+            request);
     }
 
     private ResponseEntity<Object> handleExceptionInternal(Exception e, Code errorCode,
