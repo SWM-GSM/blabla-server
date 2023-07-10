6bc955b6631abc013a2dd4ef3857e266d0ec0bad
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-07-10 22:06:25 +0900

    feat: 희망 크루원 조회 API 구현

--- src/main/java/com/gsm/blabla/common/api/CommonController.java
+++ src/main/java/com/gsm/blabla/common/api/CommonController.java
@@ -1,19 +1,13 @@
 package com.gsm.blabla.common.api;
 
 import com.gsm.blabla.common.application.CommonService;
-import com.gsm.blabla.common.dto.CrewTagDto;
-import com.gsm.blabla.common.dto.KeywordDto;
+import com.gsm.blabla.common.dto.CommonCodeDto;
 import com.gsm.blabla.global.response.DataResponseDto;
-import com.gsm.blabla.common.enums.Keyword;
-import com.gsm.blabla.common.enums.Level;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,14 +29,21 @@ public class CommonController {
     @Operation(summary = "키워드 조회 API")
     @Parameter(name = "language", description = "언어", example = "ko / en")
     @GetMapping("/{language}/common/keywords")
-    public DataResponseDto<Map<String, List<KeywordDto>>> getKeywords(@PathVariable String language) {
+    public DataResponseDto<Map<String, List<CommonCodeDto>>> getKeywords(@PathVariable String language) {
         return DataResponseDto.of(commonService.getKeywords(language));
     }
 
     @Operation(summary = "크루 태그 조회 API")
     @Parameter(name = "language", description = "언어", example = "ko / en")
     @GetMapping("/{language}/common/crew-tags")
-    public DataResponseDto<Map<String, List<CrewTagDto>>> getCrewTags(@PathVariable String language) {
+    public DataResponseDto<Map<String, List<CommonCodeDto>>> getCrewTags(@PathVariable String language) {
         return DataResponseDto.of(commonService.getCrewTags(language));
+    }
+
+    @Operation(summary = "희망 크루원 조회 API")
+    @Parameter(name = "language", description = "언어", example = "ko / en")
+    @GetMapping("/{language}/common/prefer-members")
+    public DataResponseDto<Map<String, List<CommonCodeDto>>> getPreferMembers(@PathVariable String language) {
+        return DataResponseDto.of(commonService.getPreferMembers(language));
     }
 }
