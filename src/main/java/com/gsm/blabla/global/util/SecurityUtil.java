80eb7cc875e1ddd282e9f64cef6c0b36a6e85372
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-07-11 21:29:58 +0900

    refactor: GeneralException을 반환하도록 예외 처리 방식 변경

--- src/main/java/com/gsm/blabla/global/util/SecurityUtil.java
+++ src/main/java/com/gsm/blabla/global/util/SecurityUtil.java
@@ -1,22 +1,26 @@
 package com.gsm.blabla.global.util;
 
-import lombok.NoArgsConstructor;
+import com.gsm.blabla.global.exception.GeneralException;
+import com.gsm.blabla.global.response.Code;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 @Slf4j
-@NoArgsConstructor
 public class SecurityUtil {
-    public static Long getCurrentMemberId() {
+
+    private SecurityUtil() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static Long getMemberId() {
         // Request가 들어올 때 JwtFilter의 doFilter에서 저장
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        if (authentication == null || authentication.getName() == null) {
-            throw new RuntimeException("Security Context에 인증 정보가 없습니다.");
+        if (authentication == null || authentication.getName() == null || authentication.getName().equals("anonymousUser")) {
+            throw new GeneralException(Code.NO_CREDENTIALS_IN_CONETEXT, "Security Context에 인증 정보가 없습니다.");
         }
 
-        // memberId를 저장했기 때문에 Long 타입
         return Long.parseLong(authentication.getName());
     }
 }
