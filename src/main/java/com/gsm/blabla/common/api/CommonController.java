142d7009ee0ab42636b0401643c2a6b952268356
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-07-12 19:11:15 +0900

    refactor: RequestMapping 어노테이션으로 base URL 설정

--- src/main/java/com/gsm/blabla/common/api/CommonController.java
+++ src/main/java/com/gsm/blabla/common/api/CommonController.java
@@ -12,38 +12,40 @@ import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "공통 코드 관련 API")
 @RestController
+@RequestMapping("/{language}/common")
 @RequiredArgsConstructor
 public class CommonController {
     private final CommonService commonService;
 
     @Operation(summary = "레벨 별 문구 조회 API")
     @Parameter(name = "language", description = "언어", example = "ko / en")
-    @GetMapping("{language}/common/levels")
+    @GetMapping("/levels")
     public DataResponseDto<Map<String, String>> getLevels(@PathVariable String language) {
         return DataResponseDto.of(commonService.getLevels(language));
     }
 
     @Operation(summary = "키워드 조회 API")
     @Parameter(name = "language", description = "언어", example = "ko / en")
-    @GetMapping("/{language}/common/keywords")
+    @GetMapping("/keywords")
     public DataResponseDto<Map<String, List<KeywordDto>>> getKeywords(@PathVariable String language) {
         return DataResponseDto.of(commonService.getKeywords(language));
     }
 
     @Operation(summary = "크루 태그 조회 API")
     @Parameter(name = "language", description = "언어", example = "ko / en")
-    @GetMapping("/{language}/common/crew-tags")
+    @GetMapping("/crew-tags")
     public DataResponseDto<Map<String, List<CommonCodeDto>>> getCrewTags(@PathVariable String language) {
         return DataResponseDto.of(commonService.getCrewTags(language));
     }
 
     @Operation(summary = "희망 크루원 조회 API")
     @Parameter(name = "language", description = "언어", example = "ko / en")
-    @GetMapping("/{language}/common/prefer-members")
+    @GetMapping("/prefer-members")
     public DataResponseDto<Map<String, List<CommonCodeDto>>> getPreferMembers(@PathVariable String language) {
         return DataResponseDto.of(commonService.getPreferMembers(language));
     }
