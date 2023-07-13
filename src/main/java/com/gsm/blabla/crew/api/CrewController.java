89b3426e93f62fe71be9c9bf5cab7b549e942059
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-07-14 01:22:53 +0900

    fix: 크루 목록 조회 API 최근 생성 순으로 정렬 조건 추가

--- src/main/java/com/gsm/blabla/crew/api/CrewController.java
+++ src/main/java/com/gsm/blabla/crew/api/CrewController.java
@@ -11,6 +11,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -49,7 +50,7 @@ public class CrewController {
     @GetMapping(value = "/{language}/crews")
     public DataResponseDto<Page<CrewResponseDto>> getAll(
         @PathVariable("language") String language,
-        @PageableDefault  Pageable pageable) {
+        @PageableDefault(sort = "id", direction = Direction.DESC)  Pageable pageable) {
         return DataResponseDto.of(crewService.getAll(language, pageable));
     }
 }
