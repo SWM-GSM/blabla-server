4dd2005d19e9354e36371ed5b9a6b15df4c9199a
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-07-14 01:11:35 +0900

    fix: fetch join batch_size 설정 추가

--- src/main/java/com/gsm/blabla/crew/domain/Crew.java
+++ src/main/java/com/gsm/blabla/crew/domain/Crew.java
@@ -14,6 +14,7 @@ import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.BatchSize;
 
 @Entity
 @Getter
@@ -36,9 +37,11 @@ public class Crew extends BaseTimeEntity {
     private boolean autoApproval;
     private String coverUrl;
 
+    @BatchSize(size = 1000)
     @OneToMany(mappedBy = "crew", fetch = FetchType.LAZY)
     List<CrewMember> crewMembers;
 
+    @BatchSize(size = 1000)
     @OneToMany(mappedBy = "crew", fetch = FetchType.LAZY)
     List<CrewTag> crewTags;
 
