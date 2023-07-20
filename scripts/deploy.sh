4ed8a2bd7bd8c08c82551b5a4dfecf0479490c0d
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-07-20 14:37:52 +0900

    deploy: CodeDeploy 스크립트 작성 및 Github Actions와 연동

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+REPOSITORY=/home/ubuntu/app
+cd $REPOSITORY
+
+APP_NAME=blabla
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'blabla' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> $JAR_PATH 배포"
+nohup java -jar \
+      -Dspring.profile.active=dev \
+      build/libs/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
