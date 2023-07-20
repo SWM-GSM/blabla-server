d2729ef8838bcc966ae367d9b3faac689aea49a1
Jeongyeon Lee <leeeeeyeonn@gmail.com>
2023-07-21 05:17:07 +0900

    feat: 해당 날짜의 일정 조회 API 및 다가오는 크루 일정 조회 API 구현 (#50)
    * style: 일정 관련 DTO 네이밍 변경
    
    * chore: git submodule 업데이트
    
    * feat: 해당 날짜의 일정 조회 API 구현
    
    * fix: 참여자 프로필 이미지 리스트 로직 수정
    
    * style: 안 쓰는 코드 및 import문 삭제
    
    * feat: 다가오는 크루 일정 조회 API 구현
    
    * refactor: 조회 API 관련 메서드에 트랜잭션 readOnly 옵션 추가

---
+++ src/main/java/com/gsm/blabla/crew/dto/ScheduleResponseDto.java
@@ -0,0 +1,42 @@
+package com.gsm.blabla.crew.dto;
+
+import com.gsm.blabla.crew.domain.Schedule;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ScheduleResponseDto {
+    private Long id;
+    private String title;
+    private String meetingTime;
+    private int dDay;
+    private List<String> profiles;
+
+    public static ScheduleResponseDto of(Long id, String title, String meetingTime, int dDay, List<String> profiles) {
+        return ScheduleResponseDto.builder()
+            .id(id)
+            .title(title)
+            .meetingTime(meetingTime)
+            .dDay(dDay)
+            .profiles(profiles)
+            .build();
+    }
+
+    public static ScheduleResponseDto of(Schedule schedule) {
+        return ScheduleResponseDto.builder()
+            .id(schedule.getId())
+            .title(schedule.getTitle())
+            .meetingTime(schedule.getMeetingTime().format(
+                DateTimeFormatter.ofPattern("M월 dd일 E요일 a h:mm")
+            ))
+            .dDay(schedule.getMeetingTime().getDayOfYear() - LocalDateTime.now().getDayOfYear())
+            .profiles(schedule.getMemberSchedules().stream().map(
+                        memberSchedule -> memberSchedule.getMember().getProfileUrl()
+                    ).toList())
+            .build();
+    }
+}
