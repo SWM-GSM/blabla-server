48bb58a804b3d2965efefdd661b850dff776879b
yth01 <yth011026@gmail.com>
2023-07-21 04:38:04 +0900

    feat: 컨텐츠 리스트 조회 API 구현

--- src/main/java/com/gsm/blabla/content/api/ContentController.java
+++ src/main/java/com/gsm/blabla/content/api/ContentController.java
@@ -1,15 +1,14 @@
 package com.gsm.blabla.content.api;
 
 import com.gsm.blabla.content.application.ContentService;
+import com.gsm.blabla.content.dto.ContentListResponseDto;
 import com.gsm.blabla.content.dto.ContentResponseDto;
-import com.gsm.blabla.crew.dto.CrewResponseDto;
 import com.gsm.blabla.global.response.DataResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -24,6 +23,12 @@ public class ContentController {
     @GetMapping("/contents/{contentId}")
     public DataResponseDto<Map<String, ContentResponseDto>> get(@PathVariable Long contentId) {
         return DataResponseDto.of(contentService.get(contentId));
+    }
+
+    @Operation(summary = "컨텐츠 전체 조회 API")
+    @GetMapping("/{language}/contents")
+    public DataResponseDto<Map<String, ContentListResponseDto>> getAll(@PathVariable String language) {
+        return DataResponseDto.of(contentService.getAll(language));
     }
 }
 
---
+++ src/main/java/com/gsm/blabla/content/dto/ContentListResponseDto.java
@@ -0,0 +1,36 @@
+package com.gsm.blabla.content.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ContentListResponseDto {
+    private double progress; // 연습실
+    private List<ContentResponseDto> contents; // 컨텐츠 리스트
+
+    public static ContentListResponseDto contentListResponse(List<ContentResponseDto> contents) {
+        int totalContents = contents.size();
+        Map<Integer, Long> completedContentsByLevel = contents.stream()
+                .filter(ContentResponseDto::isCompleted)
+                .collect(Collectors.groupingBy(ContentResponseDto::getLevel, Collectors.counting()));
+
+        long completedOverall = completedContentsByLevel.getOrDefault(10, 0L);
+        double overallProgress = (completedOverall / (double) totalContents) * 100.0;
+
+        int overallProgressPercentage = (int) Math.min(100, overallProgress);
+
+        return ContentListResponseDto.builder()
+                .progress(overallProgressPercentage)
+                .contents(contents)
+                .build();
+    }
+}
