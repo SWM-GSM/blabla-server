87b5b13378297dfc9a9ce6b33aabaa189b07228a
Jeongyeon Lee <leeeeeyeonn@gmail.com>
2023-07-24 19:53:44 +0900

    feat: 크루 가입 신청 API 구현 (#64)
    * feat: 크루 가입 신청 API 구현
    
    * feat: 가입 신청 문구 없이 가입 신청할 경우 예외 처리

--- src/main/java/com/gsm/blabla/crew/application/CrewService.java
+++ src/main/java/com/gsm/blabla/crew/application/CrewService.java
@@ -1,20 +1,23 @@
 package com.gsm.blabla.crew.application;
 
+import com.gsm.blabla.crew.dao.ApplyMessageRepository;
 import com.gsm.blabla.crew.dao.CrewMemberRepository;
 import com.gsm.blabla.crew.dao.CrewRepository;
 import com.gsm.blabla.crew.dao.CrewTagRepository;
+import com.gsm.blabla.crew.domain.ApplyMessage;
 import com.gsm.blabla.crew.domain.Crew;
 import com.gsm.blabla.crew.domain.CrewMember;
+import com.gsm.blabla.crew.domain.CrewMemberRole;
 import com.gsm.blabla.crew.domain.CrewMemberStatus;
 import com.gsm.blabla.crew.domain.CrewTag;
 import com.gsm.blabla.crew.dto.CrewRequestDto;
 import com.gsm.blabla.crew.dto.CrewResponseDto;
+import com.gsm.blabla.crew.dto.MessageRequestDto;
 import com.gsm.blabla.global.exception.GeneralException;
 import com.gsm.blabla.global.response.Code;
 import com.gsm.blabla.global.util.SecurityUtil;
 import com.gsm.blabla.member.dao.MemberRepository;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -32,6 +35,7 @@ public class CrewService {
     private final CrewTagRepository crewTagRepository;
     private final CrewMemberRepository crewMemberRepository;
     private final MemberRepository memberRepository;
+    private final ApplyMessageRepository applyMessageRepository;
 
     public Map<String, Long> create(CrewRequestDto crewRequestDto) {
         Crew crew = crewRepository.save(crewRequestDto.toEntity());
@@ -49,6 +53,7 @@ public class CrewService {
                 () -> new GeneralException(Code.MEMBER_NOT_FOUND, "존재하지 않는 유저입니다.")
             ))
             .crew(crew)
+            .role(CrewMemberRole.LEADER)
             .build()
         );
 
@@ -79,5 +84,54 @@ public class CrewService {
         return crewMembers.stream()
             .map(crewMember -> CrewResponseDto.myCrewListResponse(crewMember.getCrew(), crewMemberRepository))
             .toList();
+    }
+
+    public Map<String, String> joinCrew(Long crewId, MessageRequestDto messageRequestDto) {
+        Long memberId = SecurityUtil.getMemberId();
+        Crew crew = crewRepository.findById(crewId).orElseThrow(
+            () -> new GeneralException(Code.CREW_NOT_FOUND, "존재하지 않는 크루입니다.")
+        );
+        String message = "";
+
+        boolean isJoined = crewMemberRepository.getByCrewIdAndMemberId(crewId, memberId).isPresent();
+        if (isJoined) {
+            throw new GeneralException(Code.CREW_ALREADY_JOINED, "이미 가입한 크루입니다.");
+        }
+
+        boolean isApplied = applyMessageRepository.getByCrewIdAndMemberId(crewId, memberId).isPresent();
+        if (isApplied) {
+            throw new GeneralException(Code.CREW_ALREADY_APPLIED, "이미 신청한 크루입니다.");
+        }
+
+        boolean isAutoApproval = crew.getAutoApproval();
+        if (isAutoApproval) {
+            crewMemberRepository.save(
+                CrewMember.builder()
+                    .member(memberRepository.findById(memberId).orElseThrow(
+                    () -> new GeneralException(Code.MEMBER_NOT_FOUND, "존재하지 않는 유저입니다.")
+                ))
+                    .crew(crew)
+                    .role(CrewMemberRole.MEMBER)
+                    .build()
+            );
+            message = "가입이 완료되었습니다.";
+        } else {
+            if (messageRequestDto == null) {
+                throw new GeneralException(Code.APPLY_WITHOUT_MESSAGE, "크루에 가입하기 위해서는 참여 신청 문구가 필요합니다.");
+            }
+
+            applyMessageRepository.save(
+                ApplyMessage.builder()
+                    .message(messageRequestDto.getMessage())
+                    .crew(crew)
+                    .member(memberRepository.findById(memberId).orElseThrow(
+                        () -> new GeneralException(Code.MEMBER_NOT_FOUND, "존재하지 않는 유저입니다.")))
+                    .build()
+            );
+
+            message = "신청이 완료되었습니다.";
+        }
+
+        return Collections.singletonMap("message", message);
     }
 }
