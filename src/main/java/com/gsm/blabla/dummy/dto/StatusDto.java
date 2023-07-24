165c23abc319a7e54b597f56fbf568652193133f
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-07-24 10:34:11 +0900

    feat: 생성, 수정, 삭제 관련 dummy API 구현

--- src/main/java/com/gsm/blabla/dummy/dto/StatusDto.java
+++ src/main/java/com/gsm/blabla/dummy/dto/StatusDto.java
@@ -1,12 +1,8 @@
 package com.gsm.blabla.dummy.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class StatusDto {
     private String status;
 }
