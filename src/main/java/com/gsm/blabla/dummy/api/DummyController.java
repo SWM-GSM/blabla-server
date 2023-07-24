a415537f6de52658931d1af80255af99f124b1c5
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-07-24 10:34:38 +0900

    style: 안 쓰는 import문 제거

--- src/main/java/com/gsm/blabla/dummy/api/DummyController.java
+++ src/main/java/com/gsm/blabla/dummy/api/DummyController.java
@@ -30,7 +30,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
