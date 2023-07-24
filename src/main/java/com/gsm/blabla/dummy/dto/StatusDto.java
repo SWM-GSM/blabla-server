969f8a1e5891dcb1fe123dab26bae87539a140b7
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-07-24 10:15:10 +0900

    feat: dummy api controller와 dto 분리

---
+++ src/main/java/com/gsm/blabla/dummy/dto/StatusDto.java
@@ -0,0 +1,12 @@
+package com.gsm.blabla.dummy.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class StatusDto {
+    private String status;
+}
