89de042e9339afcaa923e15a468dca246a201b68
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-08-09 12:36:29 +0900

    test: 크루 단일 조회 API Controller 테스트 코드 작성

--- src/test/java/com/gsm/blabla/crew/api/CrewControllerTest.java
+++ src/test/java/com/gsm/blabla/crew/api/CrewControllerTest.java
@@ -2,12 +2,14 @@ package com.gsm.blabla.crew.api;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.gsm.blabla.crew.dto.CrewReportResponseDto;
 import com.gsm.blabla.global.ControllerTestSupport;
 import com.gsm.blabla.global.WithCustomMockUser;
 import com.gsm.blabla.common.enums.PreferMember;
@@ -15,8 +17,10 @@ import com.gsm.blabla.common.enums.Tag;
 import com.gsm.blabla.crew.domain.MeetingCycle;
 import com.gsm.blabla.crew.dto.CrewRequestDto;
 import com.gsm.blabla.member.dto.MemberRequestDto;
+import com.gsm.blabla.member.dto.MemberResponseDto;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -74,5 +78,57 @@ class CrewControllerTest extends ControllerTestSupport {
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.data.crewId").exists())
             ;
+    }
+
+    @DisplayName("[GET] 크루 리포트를 조회한다.")
+    @Test
+    @WithCustomMockUser
+    void getReport() throws Exception {
+        // given
+        Map<String, String> info = Map.of(
+            "createdAt", "2023.05.30 16:00",
+            "durationTime", "00:23:40"
+        );
+        List<MemberResponseDto> members = List.of(
+            MemberResponseDto.builder().id(1L).nickname("테스트1").profileImage("cat").build(),
+            MemberResponseDto.builder().id(2L).nickname("테스트2").profileImage("dog").build(),
+            MemberResponseDto.builder().id(3L).nickname("테스트3").profileImage("lion").build(),
+            MemberResponseDto.builder().id(4L).nickname("테스트4").profileImage("bear").build()
+        );
+        String bubbleChart = "www.test.com";
+        List<Map<String, Object>> keyword = List.of(
+            Map.of("name", "미국 유머", "count", 38),
+            Map.of("name", "중국 유머", "count", 28),
+            Map.of("name", "한국 유머", "count", 18),
+            Map.of("name", "영국 유머", "count", 8)
+        );
+        Map<String, Double> languageRatio = Map.of(
+            "korean", 60.8,
+            "eng", 39.2
+        );
+        List<MemberResponseDto> feedbacks = List.of(
+            MemberResponseDto.builder().nickname("테스트1").profileImage("cat").comment("테스트1").build(),
+            MemberResponseDto.builder().nickname("테스트2").profileImage("dog").comment("테스트2").build(),
+            MemberResponseDto.builder().nickname("테스트3").profileImage("lion").comment("테스트3").build(),
+            MemberResponseDto.builder().nickname("테스트4").profileImage("bear").comment("테스트4").build()
+        );
+
+        given(crewService.getReport(any(Long.class)))
+            .willReturn(CrewReportResponseDto.crewReportResponse(info, members, bubbleChart, keyword,
+                languageRatio, feedbacks));
+
+        // when // then
+        mockMvc.perform(
+            get("/crews/1/reports/1")
+        )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.data.info.createdAt").value("2023.05.30 16:00"))
+            .andExpect(jsonPath("$.data.info.durationTime").value("00:23:40"))
+            .andExpect(jsonPath("$.data.members").isArray())
+            .andExpect(jsonPath("$.data.bubbleChart").value("www.test.com"))
+            .andExpect(jsonPath("$.data.keyword").isArray())
+            .andExpect(jsonPath("$.data.languageRatio").isMap())
+            .andExpect(jsonPath("$.data.feedbacks").isArray());
     }
 }
