5ec6efc7a181ddcd22cc3bff907f10aa6a840378
TAEHYEOK YOO <yth@TAEHYEOKui-MacBookPro.local>
2023-08-10 16:15:37 +0900

    feat: 크루 리포트 생성 API 키워드 저장 로직 추가

---
+++ src/main/java/com/gsm/blabla/crew/dao/CrewReportKeywordRepository.java
@@ -0,0 +1,7 @@
+package com.gsm.blabla.crew.dao;
+
+import com.gsm.blabla.crew.domain.CrewReportKeyword;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CrewReportKeywordRepository extends JpaRepository<CrewReportKeyword, Long> {
+}
