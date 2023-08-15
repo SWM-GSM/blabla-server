338495700767816655c06ec7d3e9ffc41972a79f
Jeongyeon Lee <leeeeeyeonn@gmail.com>
2023-08-15 09:53:47 +0900

    fix: S3 key 및 timezone 변경 (#151)
    * chore: S3 key 변경
    
    * fix: Bean으로 TimeZone 자동 설정

--- src/main/java/com/gsm/blabla/BlablaApplication.java
+++ src/main/java/com/gsm/blabla/BlablaApplication.java
@@ -1,11 +1,17 @@
 package com.gsm.blabla;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlablaApplication {
 
+    @PostConstruct
+    public void init() {
+         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(BlablaApplication.class, args);
     }
