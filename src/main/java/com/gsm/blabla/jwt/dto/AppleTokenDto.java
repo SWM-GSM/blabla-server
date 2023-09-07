27613c652fec7fb91732b2d74cfd4fd0c5a79ebd
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-09-08 00:18:35 +0900

    fix: Snake case로 된 DTO를 Camel case로 변환

--- src/main/java/com/gsm/blabla/jwt/dto/AppleTokenDto.java
+++ src/main/java/com/gsm/blabla/jwt/dto/AppleTokenDto.java
@@ -1,13 +1,18 @@
 package com.gsm.blabla.jwt.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class AppleTokenDto {
 
-    String access_token;
-    String refresh_token;
-    String id_token;
+    String accessToken;
+    String refreshToken;
+    String idToken;
 }
