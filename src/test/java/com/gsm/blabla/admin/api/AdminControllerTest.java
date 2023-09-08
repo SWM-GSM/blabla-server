3568bef7913366ade219d39aee7838c39087dd72
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-09-08 23:18:57 +0900

    fix: health check API 테스트 코드의 display name을 직관적으로 변경

--- src/test/java/com/gsm/blabla/admin/api/AdminControllerTest.java
+++ src/test/java/com/gsm/blabla/admin/api/AdminControllerTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 
 class AdminControllerTest extends ControllerTestSupport {
 
-    @DisplayName("[GET] health check 여부를 확인한다.")
+    @DisplayName("[GET] 서버 정상 동작 시, health check가 통과한다.")
     @Test
     @WithCustomMockUser
     void getHealthCheck() throws Exception {
