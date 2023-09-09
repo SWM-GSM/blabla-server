b0142f7321cc61627ced27cd1a36faa4fd2cb7a0
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-09-10 00:59:51 +0900

    remove: 사용하지 않는 import문 제거

--- src/main/java/com/gsm/blabla/auth/api/AuthController.java
+++ src/main/java/com/gsm/blabla/auth/api/AuthController.java
@@ -8,7 +8,6 @@ import com.gsm.blabla.member.domain.SocialLoginType;
 import com.gsm.blabla.member.dto.MemberRequestDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
