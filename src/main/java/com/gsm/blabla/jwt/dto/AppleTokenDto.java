2cadcb9f5a5e75b9fa41453c5496a50aad6ed7a1
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-09-10 01:00:58 +0900

    test: 회원가입 API Business Layer 테스트 코드 작성

--- src/main/java/com/gsm/blabla/jwt/dto/AppleTokenDto.java
+++ src/main/java/com/gsm/blabla/jwt/dto/AppleTokenDto.java
@@ -3,9 +3,11 @@ package com.gsm.blabla.jwt.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class AppleTokenDto {
--- src/main/java/com/gsm/blabla/jwt/dto/GoogleAccountDto.java
+++ src/main/java/com/gsm/blabla/jwt/dto/GoogleAccountDto.java
@@ -2,9 +2,11 @@ package com.gsm.blabla.jwt.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 @JsonInclude(Include.NON_NULL)
 public class GoogleAccountDto {
     private String id;
