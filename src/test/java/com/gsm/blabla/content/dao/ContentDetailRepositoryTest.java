7fbd22f1aea1ccb038be753fd042b666ce648f79
TAEHYEOK YOO <yth@TAEHYEOKui-MacBookPro.local>
2023-09-11 00:51:26 +0900

    refactor: Persistence layer 테스트 메서드 분리

--- src/test/java/com/gsm/blabla/content/dao/ContentDetailRepositoryTest.java
+++ src/test/java/com/gsm/blabla/content/dao/ContentDetailRepositoryTest.java
@@ -34,53 +34,13 @@ class ContentDetailRepositoryTest {
     @Test
     void findAllByContent() {
         // given
-        Content content1 = Content.builder()
-            .title("인턴")
-            .description("영화 인턴을 통해 비즈니스 표현을 배워봅시다.")
-            .language("en")
-            .thumbnailURL("https://img.youtube.com/vi/sHpGT4SQwgw/hqdefault.jpg")
-            .build();
-        Content content2 = Content.builder()
-                .title("셜록")
-                .description("셜록을 통해 영국 억양과 일상 표현을 배워봅시다.")
-                .language("en")
-                .thumbnailURL("https://img.youtube.com/vi/sHpGT4SQwgw/hqdefault.jpg")
-                .build();
+        Content content1 = createContent("인턴", "영화 인턴을 통해 비즈니스 표현을 배워봅시다.", "en");
+        Content content2 = createContent("셜록", "셜록을 통해 영국 억양과 일상 표현을 배워봅시다.", "en");
         contentRepository.saveAll(List.of(content1, content2));
 
-        ContentDetail contentDetail1 = ContentDetail.builder()
-            .content(content1)
-            .title("소개 인사하기")
-            .description("인턴을 통해 소개 인사를 배워봅시다.")
-            .contentUrl("https://www.youtube.com/watch?v=sHpGT4SQwgw")
-            .guideSentence("나는 오스틴 입니다. About the Fit의 창업자 입니다.")
-            .targetSentence("I'm Jules Ostin. I'm the founder of About the Fit.")
-            .startedAt(LocalTime.of(12, 0, 0))
-            .stoppedAt(LocalTime.of(12, 0, 10))
-            .endedAt(LocalTime.of(12, 0, 20))
-            .build();
-        ContentDetail contentDetail2 = ContentDetail.builder()
-                .content(content1)
-                .title("조언해주기")
-                .description("인턴을 통해 조언해주기를 배워봅시다.")
-                .contentUrl("https://www.youtube.com/watch?v=sHpGT4SQwgw")
-                .guideSentence("나는 오스틴 입니다. About the Fit의 창업자 입니다.")
-                .targetSentence("I'm Jules Ostin. I'm the founder of About the Fit.")
-                .startedAt(LocalTime.of(13, 0, 0))
-                .stoppedAt(LocalTime.of(13, 0, 10))
-                .endedAt(LocalTime.of(13, 0, 20))
-                .build();
-        ContentDetail contentDetail3 = ContentDetail.builder()
-                .content(content2)
-                .title("인사 배우기")
-                .description("셜록을 통해 일상 표현을 배워봅시다.")
-                .contentUrl("https://www.youtube.com/watch?v=sHpGT4SQwgw")
-                .guideSentence("나는 오스틴 입니다. About the Fit의 창업자 입니다.")
-                .targetSentence("I'm Jules Ostin. I'm the founder of About the Fit.")
-                .startedAt(LocalTime.of(7, 0, 0))
-                .stoppedAt(LocalTime.of(7, 0, 10))
-                .endedAt(LocalTime.of(7, 0, 20))
-                .build();
+        ContentDetail contentDetail1 = getContentDetail(content1, "소개 인사하기", "인턴을 통해 소개 인사를 배워봅시다.", LocalTime.of(12, 0, 0), LocalTime.of(12, 0, 10), LocalTime.of(12, 0, 20));
+        ContentDetail contentDetail2 = getContentDetail(content1, "조언해주기", "인턴을 통해 조언해주기를 배워봅시다.", LocalTime.of(13, 0, 0), LocalTime.of(13, 0, 10), LocalTime.of(13, 0, 20));
+        ContentDetail contentDetail3 = getContentDetail(content2, "인사 배우기", "셜록을 통해 일상 표현을 배워봅시다.", LocalTime.of(14, 0, 0), LocalTime.of(14, 0, 10), LocalTime.of(14, 0, 20));
         contentDetailRepository.saveAll(List.of(contentDetail1, contentDetail2, contentDetail3));
 
         // when
@@ -93,6 +53,30 @@ class ContentDetailRepositoryTest {
                         tuple("소개 인사하기", "인턴을 통해 소개 인사를 배워봅시다.", LocalTime.of(12, 0, 0), LocalTime.of(12, 0, 10), LocalTime.of(12, 0, 20)),
                         tuple("조언해주기", "인턴을 통해 조언해주기를 배워봅시다.", LocalTime.of(13, 0, 0), LocalTime.of(13, 0, 10), LocalTime.of(13, 0, 20))
                 );
+    }
+
+    private static ContentDetail getContentDetail(Content content, String title, String description, LocalTime startedAt, LocalTime stoppedAt, LocalTime endedAt) {
+        ContentDetail contentDetail = ContentDetail.builder()
+            .content(content)
+            .title(title)
+            .description(description)
+            .contentUrl("https://www.youtube.com/watch?v=sHpGT4SQwgw")
+            .guideSentence("나는 오스틴 입니다. About the Fit의 창업자 입니다.")
+            .targetSentence("I'm Jules Ostin. I'm the founder of About the Fit.")
+            .startedAt(startedAt)
+            .stoppedAt(stoppedAt)
+            .endedAt(endedAt)
+            .build();
+        return contentDetail;
+    }
+
+    private static Content createContent(String title, String description, String language) {
+        return Content.builder()
+                .title(title)
+                .description(description)
+                .language(language)
+                .thumbnailURL("https://img.youtube.com/vi/sHpGT4SQwgw/hqdefault.jpg")
+                .build();
     }
 
 }
