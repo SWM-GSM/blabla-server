783c347ec8b9cf8162a2a0459b4534e7403dfa56
yth01 <yth011026@gmail.com>
2023-09-11 00:06:03 +0900

    style: 컨텐츠 API practiceService 변수명 contentService로 변경

--- src/main/java/com/gsm/blabla/content/api/ContentController.java
+++ src/main/java/com/gsm/blabla/content/api/ContentController.java
@@ -22,28 +22,28 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class ContentController {
 
-    private final ContentService practiceService;
+    private final ContentService contentService;
 
     @Operation(summary = "컨텐츠 전체 조회 API")
     @GetMapping("/contents")
     public DataResponseDto<Map<String, List<ContentsResponseDto>>> getContents(
             @Pattern(regexp = "^(ko|en)$", message = "언어는 ko 또는 en 중 하나여야 합니다.")
             @RequestHeader(name="Content-Language") String language) {
-        return DataResponseDto.of(practiceService.getContents(language));
+        return DataResponseDto.of(contentService.getContents(language));
     }
 
     @Operation(summary = "세부 컨텐츠 전체 조회 API")
     @GetMapping("/contents/{contentId}")
     public DataResponseDto<ContentDetailsResponseDto> getContentDetails(
             @Positive(message = "contentId는 양수여야 합니다.") @PathVariable Long contentId) {
-        return DataResponseDto.of(practiceService.getContentDetails(contentId));
+        return DataResponseDto.of(contentService.getContentDetails(contentId));
     }
 
     @Operation(summary = "세부 컨텐츠 단일 조회 API")
     @GetMapping("/contents/detail/{contentDetailId}")
     public DataResponseDto<ContentDetailResponseDto> getContentDetail(
             @Positive(message = "contentDetailId는 양수여야 합니다.") @PathVariable Long contentDetailId) {
-        return DataResponseDto.of(practiceService.getContentDetail(contentDetailId));
+        return DataResponseDto.of(contentService.getContentDetail(contentDetailId));
     }
 
     @Operation(summary = "연습실 피드백 생성 & 조회 API")
@@ -51,14 +51,14 @@ public class ContentController {
     public DataResponseDto<PracticeFeedbackResponseDto> createFeedback(
             @PathVariable Long contentDetailId,
             @RequestBody UserAnswerRequestDto userAnswerRequestDto) {
-        return DataResponseDto.of(practiceService.createFeedback(contentDetailId, userAnswerRequestDto));
+        return DataResponseDto.of(contentService.createFeedback(contentDetailId, userAnswerRequestDto));
     }
 
     @Operation(summary = "연습실 피드백 조회 API")
     @GetMapping("/contents/detail/{contentDetailId}/feedback")
     public DataResponseDto<PracticeFeedbackResponseDto> getFeedback(
             @PathVariable Long contentDetailId) {
-        return DataResponseDto.of(practiceService.getFeedback(contentDetailId));
+        return DataResponseDto.of(contentService.getFeedback(contentDetailId));
     }
 
     @Operation(summary = "연습 기록 음성 파일 저장 API")
@@ -66,7 +66,7 @@ public class ContentController {
     public DataResponseDto<Map<String, String>> createPracticeHistory(
             @PathVariable Long contentId,
             @RequestParam("files") List<MultipartFile> files) {
-        return DataResponseDto.of(practiceService.savePracticeHistory(contentId, files));
+        return DataResponseDto.of(contentService.savePracticeHistory(contentId, files));
     }
 }
 
