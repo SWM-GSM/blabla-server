55b758f509f1c04e321f4f68075510514d9e829e
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-09-14 19:20:16 +0900

    fix: 프론트 요청에 따른 히스토리 조회 API response 변경 및 코드 모듈화

--- src/main/java/com/gsm/blabla/report/application/ReportService.java
+++ src/main/java/com/gsm/blabla/report/application/ReportService.java
@@ -5,9 +5,12 @@ import com.gsm.blabla.crew.dao.VoiceFileRepository;
 import com.gsm.blabla.crew.domain.CrewReport;
 import com.gsm.blabla.crew.domain.CrewReportAnalysis;
 import com.gsm.blabla.crew.domain.VoiceFile;
+import com.gsm.blabla.global.exception.GeneralException;
+import com.gsm.blabla.global.response.Code;
 import com.gsm.blabla.global.util.SecurityUtil;
 import com.gsm.blabla.content.dao.MemberContentDetailRepository;
 import com.gsm.blabla.content.domain.MemberContentDetail;
+import com.gsm.blabla.member.dao.MemberRepository;
 import com.gsm.blabla.report.dto.HistoryReportResponseDto;
 import com.gsm.blabla.report.dto.HistoryResponseDto;
 import java.time.Duration;
@@ -34,6 +37,7 @@ public class ReportService {
     private final VoiceFileRepository voiceFileRepository;
     private final CrewReportAnalysisRepository crewReportAnalysisRepository;
     private final MemberContentDetailRepository memberContentRepository;
+    private final MemberRepository memberRepository;
 
     public Map<String, List<HistoryResponseDto>> getHistory() {
         Long memberId = SecurityUtil.getMemberId();
@@ -41,7 +45,7 @@ public class ReportService {
         List<HistoryResponseDto> histories = new ArrayList<>();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-        // 1. 내가 이제까지 참여한 크루 리포트 정보를 가져온다. (리포트 생성 날짜, 리포트 아이디, 크루 이름, 보이스룸 진행 시간)
+        // 1. 내가 참여한 크루 리포트 정보를 가져온다. (리포트 생성 날짜, 리포트 아이디, 크루 이름, 보이스룸 진행 시간)
         List<CrewReport> crewReports = voiceFileRepository.findAllByMemberId(memberId).stream()
             .map(VoiceFile::getCrewReport)
             .distinct()
@@ -50,58 +54,24 @@ public class ReportService {
         List<HistoryReportResponseDto> crewHistory = new ArrayList<>();
         for (CrewReport crewReport : crewReports) {
             Optional<CrewReportAnalysis> crewReportAnalysis = crewReportAnalysisRepository.findByCrewReport(crewReport);
+            // 내가 참여한 크루 리포트 정보가 있을 경우 crewHistory 리스트에 삽입
             crewReportAnalysis.ifPresent(
-                analysis -> {
-                    String durationTime = "2000-01-01 00:00";
-                    if (!Objects.equals(crewReport.getEndAt(), LocalDateTime.of(2000, 1, 1, 0, 0, 0))) {
-                        Duration duration = Duration.between(crewReport.getStartedAt(), crewReport.getEndAt());
-                        durationTime = String.format("%02d:%02d:%02d", duration.toHours(), duration.toMinutesPart(), duration.toSecondsPart());
-                    }
-
-                    String datetime = analysis.getCreatedAt().format(formatter);
-                    crewHistory.add(
-                        HistoryReportResponseDto.builder()
-                            .id(crewReport.getId())
-                            .type("crew")
-                            .info(
-                                Map.of(
-                                    "title", "API 수정을 위한 임시 title",
-                                    "subTitle", durationTime
-                                )
-                            )
-                            .dateTime(datetime)
-                            .build()
-                    );
-                }
+                analysis -> crewHistory.add(getCrewHistoryReportResponseDto(crewReport, formatter))
             );
         }
 
-        // 2. 내가 이제까지 연습한 컨텐츠 정보를 가져온다. (생성 시각, 컨텐츠 아이디, 컨텐츠 주제, 컨텐츠 이름)
+        // 2. 내가 연습한 컨텐츠 정보를 가져온다. (생성 시각, 컨텐츠 아이디, 컨텐츠 주제, 컨텐츠 이름)
         List<MemberContentDetail> memberContentDetails = memberContentRepository.findAllByMemberId(memberId);
         List<HistoryReportResponseDto> personalHistory = new ArrayList<>();
         for (MemberContentDetail memberContentDetail : memberContentDetails) {
-            String title = memberContentDetail.getContentDetail().getContent().getTitle();
-            String datetime = memberContentDetail.getJoinedAt().format(formatter);
-            personalHistory.add(
-                HistoryReportResponseDto.builder()
-                    .id(memberContentDetail.getContentDetail().getId())
-                    .type("personal")
-                    .info(
-                        Map.of(
-                            "title", memberContentDetail.getContentDetail().getTitle(),
-                            "subTitle", title
-                        )
-                    )
-                    .dateTime(datetime)
-                    .build()
-
-            );
+            personalHistory.add(getContentHistoryReportResponseDto(memberContentDetail, formatter));
         }
 
-        // 3. 날짜별로 묶어서 최신순으로 정렬한다.
+        // 3. 크루 리포트 정보와 연습 컨텐츠 정보를 하나로 합친다.
         List<HistoryReportResponseDto> historyReports = Stream.concat(crewHistory.stream(), personalHistory.stream())
             .toList();
 
+        // 4. 날짜별로 묶어서 최신순으로 정렬한다.
         Map<String, List<HistoryReportResponseDto>> groupedReports = historyReports.stream()
             .collect(Collectors.groupingBy(HistoryReportResponseDto::getDateTime));
 
@@ -123,10 +93,58 @@ public class ReportService {
         }
 
         Collections.sort(histories, Comparator.comparing(
-            HistoryResponseDto::getDatetime,
-            Comparator.nullsLast(Comparator.naturalOrder())
-        ).reversed());
+                HistoryResponseDto::getDatetime,
+                Comparator.nullsLast(Comparator.naturalOrder())
+            ).reversed()
+        );
 
         return Map.of("histories", histories);
+    }
+
+    private HistoryReportResponseDto getCrewHistoryReportResponseDto(CrewReport crewReport, DateTimeFormatter formatter) {
+        String durationTime = "2000-01-01 00:00";
+        if (!Objects.equals(crewReport.getEndAt(), LocalDateTime.of(2000, 1, 1, 0, 0, 0))) {
+            Duration duration = Duration.between(crewReport.getStartedAt(), crewReport.getEndAt());
+            durationTime = String.format("%02d:%02d:%02d", duration.toHours(), duration.toMinutesPart(), duration.toSecondsPart());
+        }
+
+        String datetime = crewReport.getStartedAt().format(formatter); // 보이스룸 생성 시각
+        return HistoryReportResponseDto.builder()
+            .id(crewReport.getId())
+            .type("crew")
+            .info(
+                Map.of(
+                    "title", getReportTitle(crewReport),
+                    "subTitle", durationTime
+                )
+            )
+            .dateTime(datetime)
+            .build();
+    }
+
+    private HistoryReportResponseDto getContentHistoryReportResponseDto(MemberContentDetail memberContentDetail, DateTimeFormatter formatter) {
+        String title = memberContentDetail.getContentDetail().getTitle();
+        String subTitle = memberContentDetail.getContentDetail().getContent().getTitle();
+        String datetime = memberContentDetail.getJoinedAt().format(formatter);
+
+        return HistoryReportResponseDto.builder()
+            .id(memberContentDetail.getContentDetail().getId())
+            .type("personal")
+            .info(
+                Map.of(
+                    "title", title,
+                    "subTitle", subTitle
+                )
+            )
+            .dateTime(datetime)
+            .build();
+    }
+
+    private String getReportTitle(CrewReport crewReport) {
+        String starter = crewReport.getMember().getNickname();
+
+        int numberOfParticipant = crewReport.getVoiceFiles().size();
+
+        return String.format("%s 외 %d명", starter, numberOfParticipant-1);
     }
 }
