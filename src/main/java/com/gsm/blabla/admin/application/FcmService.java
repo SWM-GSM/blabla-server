007e6fd659efb14fd77b689a15b6f00611913e64
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-09-14 18:32:48 +0900

    remove: 사용하지 않는 Import문 제거

--- src/main/java/com/gsm/blabla/admin/application/FcmService.java
+++ src/main/java/com/gsm/blabla/admin/application/FcmService.java
@@ -15,7 +15,6 @@ import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
-import okhttp3.Response;
 import org.apache.http.HttpHeaders;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
