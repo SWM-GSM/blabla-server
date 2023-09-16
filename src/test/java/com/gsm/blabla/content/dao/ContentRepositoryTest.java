23f06a0e88771aada72f80de56f1bd34da30c0f8
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-09-16 20:43:24 +0900

    test: 순서 추가에 따른 Repository 테스트 코드 수정

--- src/test/java/com/gsm/blabla/content/dao/ContentRepositoryTest.java
+++ src/test/java/com/gsm/blabla/content/dao/ContentRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.gsm.blabla.content.dao;
 
 import com.gsm.blabla.content.domain.Content;
+import com.gsm.blabla.global.RepositoryTestSupport;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 
 @SpringBootTest
-class ContentRepositoryTest {
+class ContentRepositoryTest extends RepositoryTestSupport {
 
     @AfterEach
     void afterEach() {
@@ -27,22 +28,22 @@ class ContentRepositoryTest {
     @Test
     void findAllByLanguage() {
         // given
-        Content content1 = createContent("인턴", "영화 인턴을 통해 비즈니스 표현을 배워봅시다.", "en");
-        Content content2 = createContent("셜록", "셜록을 통해 영국 억양과 일상 표현을 배워봅시다.", "en");
-        Content content3 = createContent("오징어 게임", "드라마 오징어 게임을 통해 줄임말 표현을 배워봅시다.", "ko");
-        Content content4 = createContent("왕좌의 게임", "왕좌의 게임을 통해 전쟁 표현을 배워봅시다.", "en");
+        Content content1 = createContent("인턴", "영화 인턴을 통해 비즈니스 표현을 배워봅시다.", "en", 3L);
+        Content content2 = createContent("셜록", "셜록을 통해 영국 억양과 일상 표현을 배워봅시다.", "en", 2L);
+        Content content3 = createContent("오징어 게임", "드라마 오징어 게임을 통해 줄임말 표현을 배워봅시다.", "ko", 1L);
+        Content content4 = createContent("왕좌의 게임", "왕좌의 게임을 통해 전쟁 표현을 배워봅시다.", "en", 1L);
         contentRepository.saveAll(List.of(content1, content2, content3, content4));
 
         // when
-        List<Content> contents = contentRepository.findAllByLanguage("en");
+        List<Content> contents = contentRepository.findAllByLanguageOrderBySequence("en");
 
         // then
         assertThat(contents).hasSize(3)
                 .extracting("title", "description", "language")
-                .containsExactlyInAnyOrder(
-                        tuple("인턴", "영화 인턴을 통해 비즈니스 표현을 배워봅시다.", "en"),
-                        tuple("셜록", "셜록을 통해 영국 억양과 일상 표현을 배워봅시다.", "en"),
-                        tuple("왕좌의 게임", "왕좌의 게임을 통해 전쟁 표현을 배워봅시다.", "en")
+                .containsExactly(
+                    tuple("왕좌의 게임", "왕좌의 게임을 통해 전쟁 표현을 배워봅시다.", "en"),
+                    tuple("셜록", "셜록을 통해 영국 억양과 일상 표현을 배워봅시다.", "en"),
+                    tuple("인턴", "영화 인턴을 통해 비즈니스 표현을 배워봅시다.", "en")
                 );
     }
 
