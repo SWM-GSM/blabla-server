a6494e8fcdbe4e4e5db345bccb2a60c422f17a2c
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-09-16 15:49:23 +0900

    fix: AppleAccountDto에 생성자 추가

--- src/main/java/com/gsm/blabla/auth/dto/AppleAccountDto.java
+++ src/main/java/com/gsm/blabla/auth/dto/AppleAccountDto.java
@@ -3,11 +3,15 @@ package com.gsm.blabla.auth.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class AppleAccountDto {
