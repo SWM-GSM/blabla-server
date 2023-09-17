834c19b3e532a7111a35fdc647c76bfab425718b
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-09-17 17:39:18 +0900

    test: 모든 테스트코드가 통과하도록 코드 수정

--- src/main/java/com/gsm/blabla/agora/dao/VoiceRoomRepository.java
+++ src/main/java/com/gsm/blabla/agora/dao/VoiceRoomRepository.java
@@ -2,11 +2,12 @@ package com.gsm.blabla.agora.dao;
 
 import com.gsm.blabla.agora.domain.VoiceRoom;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VoiceRoomRepository extends JpaRepository<VoiceRoom, Long> {
 
     Boolean existsByMemberId(Long memberId);
     List<VoiceRoom> findAllByInVoiceRoom(Boolean inVoiceRoom);
-    VoiceRoom findByMemberId(Long memberId);
+    Optional<VoiceRoom> findByMemberId(Long memberId);
 }
