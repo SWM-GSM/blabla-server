238a20eb8997377a2d452b734810f6472c08d5c6
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-10-17 16:18:51 +0900

    refactor: 묵시적 형변환을 명시적 형변환으로 수정

--- src/main/java/com/gsm/blabla/jwt/TokenProvider.java
+++ src/main/java/com/gsm/blabla/jwt/TokenProvider.java
@@ -31,8 +31,8 @@ import org.springframework.stereotype.Component;
 public class TokenProvider {
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "Bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30분
-    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000L * 60 * 30;            // 30분
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000L * 60 * 60 * 24 * 7;  // 7일
 
     private final Key key;
 
